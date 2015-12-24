307d56163b92f24551109f1a7ef038f02e42b756
Alan Zaffetti <azco21@cox.net>
2015-12-23 20:09:28 -0500

    removed unused files

--- peertable/examples/helloworld.py
+++
@@ -1,8 +0,0 @@
-from pyruntable import peer
-import socket
-
-if __name__ == '__main__':
-    sock = socket.socket()
-    sock.connect(('127.0.0.1', 12347))
-    # x = peer.Peer(12348)
-    # x.start()
